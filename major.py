3d1aed6e7fd3194e21f44d13ae5594b5294fec9e
hemanth <84442740+hemanth700@users.noreply.github.com>
2021-06-11 22:43:40 +0530

    Update major.py

--- major.py
+++ major.py
@@ -150,8 +150,8 @@ def preprocessing(reviews):
 
 #sidebar program
 def sidebar ():
-    col1, col2, col3 = st.sidebar.beta_columns([100, 100, 3])
-    col2.image('logo.jpg', width=200)
+    col1, col2, col3 = st.sidebar.beta_columns([1, 1, 1])
+    col2.image('logo.jpg', width=100)
 
     expander = st.sidebar.beta_expander('About Us')
     expander.markdown("""In this app we have tried to do sentimental analysis on reviews in  Flipkart :
